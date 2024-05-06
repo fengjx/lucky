8f71c30089b1f3f2edede780e7e8ec737ee10944
fengjx <fengjianxin2012@gmail.com>
2024-05-06 23:00:10 +0800

    remove cms

--- logic/init.go
+++ logic/init.go
@@ -6,12 +6,10 @@ import (
 	"github.com/fengjx/luchen"
 
 	"github.com/fengjx/lucky/connom/lifecycle"
-	"github.com/fengjx/lucky/logic/cms"
 	"github.com/fengjx/lucky/logic/sys"
 )
 
 func Init(ctx context.Context, httpServer *luchen.HTTPServer) {
 	sys.Init(ctx, httpServer)
-	cms.Init(ctx, httpServer)
 	lifecycle.DoHooks()
 }
